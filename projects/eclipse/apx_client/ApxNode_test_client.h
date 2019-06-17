e36b89339fc3a46ff18e2ea1dc0d8ea57fd90b7a
cogu <congus8@gmail.com>
2019-06-17 10:15:57 +0200

    Regenerated APX node for test client

--- projects/eclipse/apx_client/ApxNode_test_client.h
+++ projects/eclipse/apx_client/ApxNode_test_client.h
@@ -1,6 +1,7 @@
 #ifndef APXNODE_TEST_CLIENT_H
 #define APXNODE_TEST_CLIENT_H
 
+#include <stdbool.h>
 #include "apx_nodeData.h"
 #include "Rte_Type.h"
 
@@ -11,12 +12,14 @@
 //////////////////////////////////////////////////////////////////////////////
 // FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////////
-void ApxNode_Init_test_client(void);
+apx_nodeData_t * ApxNode_Init_test_client(void);
 apx_nodeData_t * ApxNode_GetNodeData_test_client(void);
+bool ApxNode_IsConnected_test_client(void);
 
 Std_ReturnType ApxNode_Read_test_client_EngineRunningStatus(OffOn_T *val);
 Std_ReturnType ApxNode_Read_test_client_FuelLevelPercent(Percent_T *val);
 Std_ReturnType ApxNode_Read_test_client_VehicleSpeed(VehicleSpeed_T *val);
+Std_ReturnType ApxNode_Write_test_client_VehicleMode(uint8 val);
 void test_client_inPortDataWritten(void *arg, apx_nodeData_t *nodeData, uint32_t offset, uint32_t len);
 
 #endif //APXNODE_TEST_CLIENT_H
--- projects/eclipse/apx_client/gen_source.py
+++ projects/eclipse/apx_client/gen_source.py
@@ -2,7 +2,9 @@ import autosar
 import apx
 
 ws = autosar.workspace()
-dataTypes = ws.getDataTypePackage()
+dataTypes = ws.createPackage('DataType', role='DataType')
+dataTypes.createSubPackage('CompuMethod', role='CompuMethod')
+dataTypes.createSubPackage('Unit', role='Unit')
 dataTypes.createIntegerDataType('OffOn_T', valueTable=[
        "OffOn_Off",
        "OffOn_On",
@@ -10,22 +12,23 @@ dataTypes.createIntegerDataType('OffOn_T', valueTable=[
        "OffOn_NotAvailable"])
 dataTypes.createIntegerDataType('Percent_T', min=0, max=255, offset=0, scaling=0.4, unit='Percent')
 dataTypes.createIntegerDataType('VehicleSpeed_T', min=0, max=65535, offset=0, scaling=1/64, unit='km/h')
-constants = ws.getConstantPackage()
+constants = ws.createPackage('Constant', role='Constant')
 constants.createConstant('C_EngineRunningStatus_IV', 'OffOn_T', 3)
 constants.createConstant('C_FuelLevelPercent_IV', 'Percent_T', 255)
 constants.createConstant('C_VehicleSpeed_IV', 'VehicleSpeed_T', 65535)
-portInterfaces = ws.getPortInterfacePackage()
+portInterfaces = ws.createPackage('PortInterface', role='PortInterface')
 portInterfaces.createSenderReceiverInterface('EngineRunningStatus_I',
                                              autosar.DataElement('EngineRunningStatus', 'OffOn_T'))
 portInterfaces.createSenderReceiverInterface('FuelLevelPercent_I',
                                              autosar.DataElement('FuelLevelPercent', 'Percent_T'))
 portInterfaces.createSenderReceiverInterface('VehicleSpeed_I',
                                              autosar.DataElement('VehicleSpeed', 'VehicleSpeed_T'))
-components = ws.getComponentTypePackage()
+components = ws.createPackage('ComponentType', role='ComponentType')
 swc = components.createApplicationSoftwareComponent('test_client')
 swc.createRequirePort('EngineRunningStatus', 'EngineRunningStatus_I', initValueRef=constants['C_EngineRunningStatus_IV'].ref)
 swc.createRequirePort('FuelLevelPercent', 'FuelLevelPercent_I', initValueRef=constants['C_FuelLevelPercent_IV'].ref)
 swc.createRequirePort('VehicleSpeed', 'VehicleSpeed_I', initValueRef=constants['C_VehicleSpeed_IV'].ref)
+
 swc.behavior.createRunnable(swc.name+'_Init', portAccess=[x.name for x in swc.providePorts])
 swc.behavior.createRunnable(swc.name+'_Run', portAccess=[x.name for x in swc.requirePorts+swc.providePorts])
 
@@ -36,5 +39,6 @@ typeGenerator.generate()
 
 node = apx.Node()
 node.import_autosar_swc(swc)
+node.append(apx.ProvidePort('VehicleMode', 'C(0,15)', '=15'))
 apx.NodeGenerator().generate('.', node, includes=['Rte_Type.h'])
 apx.Context().append(node).generateAPX('.')
