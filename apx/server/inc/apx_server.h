1f0e7efec5c2b04d2381f5e612c5b6c0601dd266
sander-visser <github@visser.se>
2019-12-03 22:50:41 +0100

    Disable Unix socket server on v0.2.x branch

--- apx/server/inc/apx_server.h
+++ apx/server/inc/apx_server.h
@@ -24,9 +24,9 @@
 typedef struct apx_server_tag
 {
    uint16_t tcpPort; //TCP port for tcpServer
-   char *localServerFile; //path to socket file for unix domain sockets (used for localServer)
+   //char *localServerFile; //path to socket file for unix domain sockets (used for localServer)
    msocket_server_t tcpServer; //tcp server
-   msocket_server_t localServer; //unix domain socket server (to be implemented later)
+   //msocket_server_t localServer; //unix domain socket server (Will not be aded to v0.2.x)
    adt_list_t connections; //linked list of strong references to apx_serverConnection_t
    apx_nodeManager_t nodeManager; //the server has a single instance of the node manager, all connections interface with this object
    apx_router_t router; //this component handles all routing tables within the server
@@ -42,7 +42,7 @@ typedef struct apx_server_tag
 //////////////////////////////////////////////////////////////////////////////
 // GLOBAL FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////////
-void apx_server_create(apx_server_t *self, uint16_t port);
+void apx_server_create(apx_server_t *self, uint16_t tcpPort);
 void apx_server_destroy(apx_server_t *self);
 void apx_server_start(apx_server_t *self);
 void apx_server_setDebugMode(apx_server_t *self, int8_t debugMode);
