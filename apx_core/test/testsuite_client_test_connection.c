e01ab889c2f6c36bf2e2d6033070b29ef65825f2
cogu <congus8@gmail.com>
2026-09-24 18:45:24 +0200

    Ensure port count files are always open first

--- apx_core/test/testsuite_client_test_connection.c
+++ apx_core/test/testsuite_client_test_connection.c
@@ -38,6 +38,8 @@ static void test_definition_file_is_sent_when_file_open_is_requested(CuTest* tc)
 static void test_provide_port_file_is_sent_when_file_open_requested(CuTest* tc);
 static void test_require_port_file_is_requested_when_published_by_server(CuTest* tc);
 static void test_node_data_is_updated_when_require_port_is_written(CuTest* tc);
+static void test_client_requests_open_cout_cin_before_in_file(CuTest* tc);
+static void test_client_requests_open_cout_cin_before_in_file_when_in_published_first(CuTest* tc);
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -60,6 +62,8 @@ CuSuite* testsuite_apx_client_test_connection(void)
    SUITE_ADD_TEST(suite, test_provide_port_file_is_sent_when_file_open_requested);
    SUITE_ADD_TEST(suite, test_require_port_file_is_requested_when_published_by_server);
    SUITE_ADD_TEST(suite, test_node_data_is_updated_when_require_port_is_written);
+   SUITE_ADD_TEST(suite, test_client_requests_open_cout_cin_before_in_file);
+   SUITE_ADD_TEST(suite, test_client_requests_open_cout_cin_before_in_file_when_in_published_first);
 
    return suite;
 }
@@ -321,6 +325,149 @@ static void test_node_data_is_updated_when_require_port_is_written(CuTest* tc)
    CuAssertIntEquals(tc, APX_NO_ERROR, apx_node_data_read_require_port_data(node_data, 0, &buffer[0], (apx_size_t) sizeof(buffer)));
    CuAssertUIntEquals(tc, data[0], buffer[0]);
    CuAssertUIntEquals(tc, data[1], buffer[1]);
+
+   apx_client_test_connection_delete(connection);
+}
+
+static void test_client_requests_open_cout_cin_before_in_file(CuTest* tc)
+{
+   apx_client_test_connection_t* connection;
+   adt_bytearray_t* packet;
+   char const* apx_text =
+      "APX/1.3\n"
+      "N\"TestNode13\"\n"
+      "P\"ProvidePort1\"C(0,3)\n"
+      "R\"RequirePort1\"C(0,7):=7\n";
+
+   connection = apx_client_test_connection_new();
+   CuAssertPtrNotNull(tc, connection);
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_build_node(connection, apx_text));
+   CuAssertIntEquals(tc, 0u, apx_client_test_connection_log_length(connection));
+   apx_client_test_connection_greeting_header_accepted_notification(connection);
+   apx_client_test_connection_run(connection);
+   CuAssertIntEquals(tc, 1u, apx_client_test_connection_log_length(connection));
+   apx_client_test_connection_clear_log(connection);
+
+   // Server publishes TestNode13.cout, TestNode13.cin, and TestNode13.in
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_COUNT_ADDRESS_START, "TestNode13.cout", 2u));
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_COUNT_ADDRESS_START + 0x400u, "TestNode13.cin", 2u));
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_DATA_ADDRESS_START, "TestNode13.in", 1u));
+
+   CuAssertIntEquals(tc, 0u, apx_client_test_connection_log_length(connection));
+   apx_client_test_connection_run(connection);
+
+   // Verify that 3 open requests were queued in exact order: .cout, .cin, .in
+   uint32_t opened_addresses[3] = {0};
+   int msg_count = 0;
+   for (int32_t i = 0; i < apx_client_test_connection_log_length(connection); ++i)
+   {
+      packet = apx_client_test_connection_get_log_packet(connection, i);
+      CuAssertPtrNotNull(tc, packet);
+      uint8_t const* p_data = adt_bytearray_const_data(packet);
+      int p_len = (int)adt_bytearray_length(packet);
+      while (p_len > 0)
+      {
+         uint32_t msg_len = 0;
+         uint8_t const* next = numheader_decode32(p_data, p_data + p_len, &msg_len);
+         CuAssertPtrNotNull(tc, next);
+         p_len -= (int)(next - p_data);
+         p_data = next;
+         CuAssertTrue(tc, p_len >= (int)msg_len);
+
+         uint32_t decoded_address = 0;
+         bool more_bit = false;
+         apx_size_t h_size = rmf_address_decode(p_data, p_data + msg_len, &decoded_address, &more_bit);
+         CuAssertTrue(tc, h_size > 0);
+         CuAssertUIntEquals(tc, RMF_CMD_AREA_START_ADDRESS, decoded_address);
+
+         uint8_t const* cmd_data = p_data + h_size;
+         uint32_t cmd_type = unpackLE(cmd_data, UINT32_SIZE);
+         uint32_t file_address = unpackLE(cmd_data + UINT32_SIZE, UINT32_SIZE);
+         CuAssertUIntEquals(tc, RMF_CMD_OPEN_FILE_MSG, cmd_type);
+
+         if (msg_count < 3)
+         {
+            opened_addresses[msg_count] = file_address;
+         }
+         msg_count++;
+         p_data += msg_len;
+         p_len -= (int)msg_len;
+      }
+   }
+   CuAssertIntEquals(tc, 3, msg_count);
+   CuAssertUIntEquals(tc, APX_PORT_COUNT_ADDRESS_START, opened_addresses[0]);
+   CuAssertUIntEquals(tc, APX_PORT_COUNT_ADDRESS_START + 0x400u, opened_addresses[1]);
+   CuAssertUIntEquals(tc, APX_PORT_DATA_ADDRESS_START, opened_addresses[2]);
+
+   apx_client_test_connection_delete(connection);
+}
+
+static void test_client_requests_open_cout_cin_before_in_file_when_in_published_first(CuTest* tc)
+{
+   apx_client_test_connection_t* connection;
+   adt_bytearray_t* packet;
+   char const* apx_text =
+      "APX/1.3\n"
+      "N\"TestNode13\"\n"
+      "P\"ProvidePort1\"C(0,3)\n"
+      "R\"RequirePort1\"C(0,7):=7\n";
+
+   connection = apx_client_test_connection_new();
+   CuAssertPtrNotNull(tc, connection);
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_build_node(connection, apx_text));
+   apx_client_test_connection_greeting_header_accepted_notification(connection);
+   apx_client_test_connection_run(connection);
+   apx_client_test_connection_clear_log(connection);
+
+   // Server has .cout and .cin published in file map, but sends publish .in first
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_COUNT_ADDRESS_START, "TestNode13.cout", 2u));
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_COUNT_ADDRESS_START + 0x400u, "TestNode13.cin", 2u));
+   CuAssertIntEquals(tc, APX_NO_ERROR, apx_client_test_connection_publish_remote_file(connection, APX_PORT_DATA_ADDRESS_START, "TestNode13.in", 1u));
+
+   apx_client_test_connection_run(connection);
+
+   // Verify all 3 are requested open, and .cout and .cin are opened before .in
+   uint32_t opened_addresses[3] = {0};
+   int msg_count = 0;
+   for (int32_t i = 0; i < apx_client_test_connection_log_length(connection); ++i)
+   {
+      packet = apx_client_test_connection_get_log_packet(connection, i);
+      CuAssertPtrNotNull(tc, packet);
+      uint8_t const* p_data = adt_bytearray_const_data(packet);
+      int p_len = (int)adt_bytearray_length(packet);
+      while (p_len > 0)
+      {
+         uint32_t msg_len = 0;
+         uint8_t const* next = numheader_decode32(p_data, p_data + p_len, &msg_len);
+         CuAssertPtrNotNull(tc, next);
+         p_len -= (int)(next - p_data);
+         p_data = next;
+         CuAssertTrue(tc, p_len >= (int)msg_len);
+
+         uint32_t decoded_address = 0;
+         bool more_bit = false;
+         apx_size_t h_size = rmf_address_decode(p_data, p_data + msg_len, &decoded_address, &more_bit);
+         CuAssertTrue(tc, h_size > 0);
+         CuAssertUIntEquals(tc, RMF_CMD_AREA_START_ADDRESS, decoded_address);
+
+         uint8_t const* cmd_data = p_data + h_size;
+         uint32_t cmd_type = unpackLE(cmd_data, UINT32_SIZE);
+         uint32_t file_address = unpackLE(cmd_data + UINT32_SIZE, UINT32_SIZE);
+         CuAssertUIntEquals(tc, RMF_CMD_OPEN_FILE_MSG, cmd_type);
+
+         if (msg_count < 3)
+         {
+            opened_addresses[msg_count] = file_address;
+         }
+         msg_count++;
+         p_data += msg_len;
+         p_len -= (int)msg_len;
+      }
+   }
+   CuAssertIntEquals(tc, 3, msg_count);
+   CuAssertUIntEquals(tc, APX_PORT_COUNT_ADDRESS_START, opened_addresses[0]);
+   CuAssertUIntEquals(tc, APX_PORT_COUNT_ADDRESS_START + 0x400u, opened_addresses[1]);
+   CuAssertUIntEquals(tc, APX_PORT_DATA_ADDRESS_START, opened_addresses[2]);
 
    apx_client_test_connection_delete(connection);
 }
